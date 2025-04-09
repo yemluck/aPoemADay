5fe38c8ed56e0239cab0389354401cd9f464cae3
Chris Agbolade <yemluck@gmail.com>
2025-04-09 00:44:01 -0500

    created the database file and updated the script to run the database

---
+++ database.sql
@@ -0,0 +1,18 @@
+-- USER is a reserved keyword with Postgres
+-- You must use double quotes in every query that user is in:
+-- ex. SELECT * FROM "user";
+-- Otherwise you will have errors!
+-- Database name is apoemaday
+
+CREATE TABLE "user" (
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR (20) UNIQUE NOT NULL,
+  "user_type" VARCHAR (10) NOT NULL,
+);
+
+CREATE TABLE "poems" (
+  "id" SERIAL PRIMARY KEY,
+  "title" VARCHAR (100) NOT NULL,
+  "author" VARCHAR (100) NOT NULL,
+  "poem" VARCHAR (100000) NOT NULL,
+)
