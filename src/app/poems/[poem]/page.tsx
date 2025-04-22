2fa9a68f471521f27ef57497c0ce26d176f89dd1
Chris Agbolade <yemluck@gmail.com>
2025-04-22 01:55:23 -0500

    fixed bad route in dynami route

--- src/app/poems/[poem]/page.tsx
+++ src/app/poems/[poem]/page.tsx
@@ -1,17 +1,20 @@
 import pool from "@/app/lib/server/db";
 import Link from "next/link";
-import NotFound from "../../../../not-found";
+import { notFound } from "next/navigation";
 
 export default async function PoemDetails({ params }) {
   // console.log("this is the params", await params.poem);
 
   const { poem } = params;
 
-  if (poem == "undefined") return NotFound;
-
   const poemDetail = await pool
     .query(`SELECT * FROM "poems" WHERE "id" = ${poem}`)
     .then((res) => res.rows[0]);
+
+  // if bad routes is entered, call not found function
+  if (!poemDetail) {
+    notFound(); // throw 404 error if poem is not found
+  }
 
   return (
     <>
